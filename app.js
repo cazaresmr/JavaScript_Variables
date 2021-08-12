cae203a263d858e8dbd7b923252ff07ded76356a
cazaresmr <michaelcazares@gmail.com>
2021-08-11 20:24:25 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
+// console.log("Hello World!\n==========\n");
+// console.log("Follow the steps in the README.md file to complete the exercises:\n==========\n");
 
 // Exercise 1
+const fname = 'Michael';
+let lname = 'Cazares';
+var age = 67;
 
 // Exercise 2
+let fullName = fname + " " + lname;
+console.log('fullName: ' + fullName);
+console.log(`My full name is ${fullName}.`);
 
 // Exercise 3
+let myCity = "Los Angeles";
+let myFavPastTimes = 'spending time with my wife, teaching, and learning how to code.'
+let myStory = `Hello, my name is ${fullName}. I am from ${myCity}, CA. I enjoy ${myFavPastTimes}`
+console.log(myStory);
