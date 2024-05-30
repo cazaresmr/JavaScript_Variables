ab958cb0207c4536781f65c6a9a5d131d869a75f
Michael Cazares <michaelcazares@gmail.com>
2024-05-29 21:06:54 -0700

    completed 'JS: Intro and Variables' exercise

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-// console.log("Hello World!\n==========\n");
-// console.log("Follow the steps in the README.md file to complete the exercises:\n==========\n");
-
 // Exercise 1
-const fname = 'Michael';
-let lname = 'Cazares';
-var age = 67;
+let lastName;
+var age;
 
 // Exercise 2
-let fullName = fname + " " + lname;
-console.log('fullName: ' + fullName);
-console.log(`My full name is ${fullName}.`);
+const fname = "Michael";
+lastName = "Cazares";
+age = 70;
 
 // Exercise 3
-let myCity = "Los Angeles";
-let myFavPastTimes = 'spending time with my wife, teaching, and learning how to code.'
-let myStory = `Hello, my name is ${fullName}. I am from ${myCity}, CA. I enjoy ${myFavPastTimes}`
-console.log(myStory);
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectdSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
